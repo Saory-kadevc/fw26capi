4c18c51a4fa4b5eba2281cf5a10d3c8a9927ce2f
Saory-kadevc <nicolasdesouzapereira830@gmail.com>
2025-12-02 10:35:02 -0300

    Fix database connection handling in getnoticias route

--- rotas/getnoticias.js
+++ rotas/getnoticias.js
@@ -2,8 +2,7 @@ module.exports = (app)=>{
     app.get('/noticias', async (req, res)=>{
         try{
             console.log("teste1")
-            //const connect = await app.dbClient.connect();
-            console.log(connect)
+            await app.dbClient.connect();
             res.send("teste")
             
             // const resultado = await app.dbClient.db('portalnoticias')
@@ -19,3 +18,4 @@ module.exports = (app)=>{
     })
 
 }
+
