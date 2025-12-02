767e945c9ae4807f462f0db43e2cbc519ae0b8a1
Saory-kadevc <nicolasdesouzapereira830@gmail.com>
2025-12-02 09:57:47 -0300

    fix

--- rotas/gettiponoticias.js
+++ rotas/gettiponoticias.js
@@ -1,5 +1,16 @@
-module.exports=(app)=>{
-    app.get("/noticias/tipo/:tiponoticia", async (req, res)=>{
-        res.send(req.params.tiponoticia)
+module.exports = (app) => {
+    app.get("/noticias/tipo/:tiponoticia", async (req, res) => {
+        try {
+            const tiponoticia = (req.params.tiponoticia)
+            await app.dbClient.connect();
+            const resultado = await app.dbClient.db('portalnoticias')
+                .collection('noticias')
+                .find({ tiponoticia: tiponoticia })
+                .toArray();
+            res.json(resultado);
+        } catch (error) {
+            res.json(error);
+
+        }
     })
 }
